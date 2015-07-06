72c29d291a284a4248f8b89e3a45343bd0705a28
CapsarM <capsar.meijer@gmail.com>
2015-07-06 15:40:53 +0200

    Uhmm, This is the only way I got it to be setup properly sorry if I fucked stuff up for you guys. I wil help set it up for you guys!

--- java/net/spookysquad/spookster/LiteModHijacked.java
+++ java/net/spookysquad/spookster/LiteModHijacked.java
@@ -8,31 +8,32 @@ import com.mumfrey.liteloader.Tickable;
 
 /**
  * We'll use this as a template to paste into "hiding" the mod into another mod
- * i.e TabbyChat, WorldEditWrapper
+ * i.e TabbyChat, WorldEditWrapper, PotionColoriezer ;)
+ * 
  * @author TehNeon
  */
-public class HijackedLitemod implements Tickable {
+public class LiteModHijacked implements Tickable {
 
-	private SpooksterLitemod spookster;
-	
+	private Spookster spookster = new Spookster();
+
 	public String getVersion() {
 		return spookster.getVersion();
-	}
-
-	public void init(File configPath) {	
-		spookster.init(configPath);
-	}
-
-	public void upgradeSettings(String version, File configPath, File oldConfigPath) {		
-		spookster.upgradeSettings(version, configPath, oldConfigPath);
 	}
 
 	public String getName() {
 		return spookster.getName();
 	}
 
+	public void init(File configPath) {
+		spookster.init();
+	}
+
+	public void upgradeSettings(String version, File configPath, File oldConfigPath) {
+		spookster.upgradeSettings(version, configPath, oldConfigPath);
+	}
+
 	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
 		spookster.onTick(minecraft, partialTicks, inGame, clock);
 	}
-	
+
 }
--- java/net/spookysquad/spookster/Spookster.java
+++ java/net/spookysquad/spookster/Spookster.java
@@ -1,7 +1,9 @@
 package net.spookysquad.spookster;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import net.minecraft.client.Minecraft;
 import net.spookysquad.spookster.event.EventManager;
 import net.spookysquad.spookster.manager.Manager;
 import net.spookysquad.spookster.mod.ModuleManager;
@@ -10,27 +12,42 @@ public class Spookster {
 
 	private ArrayList<Manager> managers = new ArrayList<Manager>();
 	public static Spookster instance;
-	
+
 	public EventManager eventManager;
 	public ModuleManager moduleManager;
-	
+
 	public void init() {
 		instance = this;
 		managers.add(eventManager = new EventManager());
 		managers.add(moduleManager = new ModuleManager());
-		
-		for(Manager manager: managers) {
+
+		for (Manager manager : managers) {
 			manager.init(this);
 		}
-		
-		
+
 		Runtime.getRuntime().addShutdownHook(new Thread("Shutdown Thread") {
 			@Override
 			public void run() {
-				for(Manager manager: managers) {
+				for (Manager manager : managers) {
 					manager.deinit(Spookster.this);
 				}
 			}
 		});
+	}
+
+	public String getVersion() {
+		return "v0.0";
+	}
+
+	public String getName() {
+		return "LiteAPI";
+	}
+
+	public void upgradeSettings(String version, File configPath, File oldConfigPath) {
+
+	}
+
+	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
+
 	}
 }
