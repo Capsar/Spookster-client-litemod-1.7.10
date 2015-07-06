ab6bd08d8755c48a991438bacde3a79bb8f75749
TehNeon <tehneon1212@gmail.com>
2015-07-05 21:22:58 -0400

    Started Client Base Base Super Base. (LiteMod base)

---
+++ net/spookysquad/spookster/HijackedLitemod.java
@@ -0,0 +1,33 @@
+package net.spookysquad.spookster;
+
+import java.io.File;
+
+import net.minecraft.client.Minecraft;
+
+import com.mumfrey.liteloader.Tickable;
+
+public class HijackedLitemod implements Tickable {
+
+	private Spookster spookster;
+	
+	public String getVersion() {
+		return spookster.getVersion();
+	}
+
+	public void init(File configPath) {	
+		spookster.init(configPath);
+	}
+
+	public void upgradeSettings(String version, File configPath, File oldConfigPath) {		
+		spookster.upgradeSettings(version, configPath, oldConfigPath);
+	}
+
+	public String getName() {
+		return spookster.getName();
+	}
+
+	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
+		spookster.onTick(minecraft, partialTicks, inGame, clock);
+	}
+	
+}
--- net/spookysquad/spookster/Spookster.java
+++ net/spookysquad/spookster/Spookster.java
@@ -1,5 +1,33 @@
 package net.spookysquad.spookster;
 
-public class Spookster {
+import java.io.File;
 
+import net.minecraft.client.Minecraft;
+
+import com.mumfrey.liteloader.Tickable;
+
+public class Spookster implements Tickable {
+
+	// Useless
+	public String getVersion() {
+		return null;
+	}
+	
+	// Useless
+	public String getName() {
+		return null;
+	}
+
+	public void init(File configPath) {	
+		
+	}
+
+	public void upgradeSettings(String version, File configPath, File oldConfigPath) {	
+		
+	}
+
+
+	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
+		
+	}
 }
