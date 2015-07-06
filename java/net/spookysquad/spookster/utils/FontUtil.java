ff6f2e1b3286a52aed685cbe7526ca0268163a71
Rederpz <rederpz@live.com>
2015-07-06 15:11:36 -0500

    HUD Overhaul

--- java/net/spookysquad/spookster/utils/FontUtil.java
+++ java/net/spookysquad/spookster/utils/FontUtil.java
@@ -13,6 +13,10 @@ public class FontUtil extends Wrapper {
     	return returnFloat;
     }
     
+    public static float drawCenteredString(String text, float x, float y, int color) {
+    	return drawString(text, x - (getFont().getStringWidth(text) / 2), y, color);
+    }
+    
     public static float drawStringWithShadow(String text, float x, float y, int color) {
     	GL11.glTranslatef(x, y, 0);
     	float returnFloat = getFont().drawString(text, 0, 0, color, true);
@@ -31,10 +35,6 @@ public class FontUtil extends Wrapper {
     	return drawStringWithShadow(text, x, y, color, width, (color & 16579836) >> 2 | color & -16777216);
     }
     
-    public static float drawCenteredString(String text, float x, float y, int color) {
-    	return drawString(text, x - (getFont().getStringWidth(text) / 2), y, color);
-    }
-    
     public static float drawCenteredStringWithShadow(String text, float x, float y, int color) {
     	return drawStringWithShadow(text, x - (getFont().getStringWidth(text) / 2), y, color);
     }
@@ -45,6 +45,33 @@ public class FontUtil extends Wrapper {
     
     public static float drawCenteredStringWithShadow(String text, float x, float y, int color, float width, int shadowColor) {
     	return drawStringWithShadow(text, x - (getFont().getStringWidth(text) / 2), y, color, width, shadowColor);
+    }
+    
+    public static float drawStringWithOutline(String text, float x, float y, int color, float width, int shadowColor) {
+    	GL11.glTranslatef(width, 0, 0);
+    	drawString(stripColorCodes(text), x, y, shadowColor);
+    	GL11.glTranslatef(0, width, 0);
+    	drawString(stripColorCodes(text), x, y, shadowColor);
+    	GL11.glTranslatef(-width, -width, 0);
+    	GL11.glTranslatef(-width , 0, 0);
+    	drawString(stripColorCodes(text), x, y, shadowColor);
+    	GL11.glTranslatef(0, -width, 0);
+    	drawString(stripColorCodes(text), x, y, shadowColor);
+    	GL11.glTranslatef(width, width, 0);
+    	
+    	return drawString(text, x, y, color);
+    }
+
+    public static float drawStringWithOutline(String text, float x, float y, int color, float width) {
+    	return drawStringWithOutline(text, x, y, color, width, (color & 16579836) >> 2 | color & -16777216);
+    }
+    
+    public static float drawCenteredStringWithOutline(String text, float x, float y, int color, float width) {
+    	return drawStringWithOutline(text, x - (getFont().getStringWidth(text) / 2), y, color, width);
+    }
+    
+    public static float drawCenteredStringWithOutline(String text, float x, float y, int color, float width, int shadowColor) {
+    	return drawStringWithOutline(text, x - (getFont().getStringWidth(text) / 2), y, color, width, shadowColor);
     }
 
     private static final Pattern patternControlCode = Pattern.compile("(?i)\\u00A7[0-9A-FK-OR]");
