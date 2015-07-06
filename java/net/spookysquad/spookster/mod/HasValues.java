e5b41780c14a607f1ac14cb94957652b19494d0b
CapsarM <capsar.meijer@gmail.com>
2015-07-06 21:43:26 +0200

    Added Stuff :0

---
+++ java/net/spookysquad/spookster/mod/HasValues.java
@@ -0,0 +1,74 @@
+package net.spookysquad.spookster.mod;
+
+import java.util.List;
+
+public interface HasValues {
+
+	public class Value {
+		public Value(String name, Object min, Object max, float increasement) {
+			super();
+			assert min.getClass().equals(max.getClass());
+			isEditable = true;
+			this.increasement = increasement;
+			this.name = name;
+			this.min = min;
+			this.max = max;
+		}
+
+		public Value(String name, Object min, Object max) {
+			super();
+			assert min.getClass().equals(max.getClass());
+			isEditable = true;
+			this.name = name;
+			this.min = min;
+			this.max = max;
+		}
+
+		public Value(String name, List list, Object type) {
+			super();
+			this.name = name;
+			this.list = list;
+			this.type = type;
+		}
+
+		private final String name;
+
+		private boolean isEditable = false;
+
+		private float increasement = 0.1F;
+
+		private List list;
+
+		private Object type, min, max;
+
+		public boolean isEditable() {
+			return isEditable;
+		}
+
+		public float getIncreasement() {
+			return increasement;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Object getMin() {
+			return min;
+		}
+
+		public Object getMax() {
+			return max;
+		}
+
+		public Class getVClass() {
+			return max.getClass();
+		}
+	}
+	
+	List<Value> getValues();
+
+	Object getValue(String n);
+
+	void setValue(String n, Object v);
+}
--- java/net/spookysquad/spookster/mod/mods/Triggerbot.java
+++ java/net/spookysquad/spookster/mod/mods/Triggerbot.java
@@ -1,21 +1,49 @@
 package net.spookysquad.spookster.mod.mods;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.spookysquad.spookster.event.Event;
 import net.spookysquad.spookster.event.events.EventPreMotion;
+import net.spookysquad.spookster.mod.HasValues;
 import net.spookysquad.spookster.mod.Module;
 import net.spookysquad.spookster.mod.Type;
+import net.spookysquad.spookster.utils.TimeUtil;
 
 import org.lwjgl.input.Keyboard;
 
-public class Triggerbot extends Module {
+public class Triggerbot extends Module implements HasValues {
 
 	public Triggerbot() {
 		super(new String[] { "Triggerbot" }, "When your cursor is over an enemy, it attacks.", Type.COMBAT, Keyboard.KEY_R, 0xFF0A9D0F);
 	}
 
+	public int aps = 5;
+	public int aimspeed = 2;
+	public int aimspeedbody = 3;
+	public double range = 4.2;
+	private static EntityPlayer livingbase;
+	TimeUtil time = new TimeUtil();
+	
 	@Override
 	public void onEvent(Event event) {
 		if(event instanceof EventPreMotion) {
+			
 		}
+	}
+
+	@Override
+	public List<Value> getValues() {
+		return null;
+	}
+
+	@Override
+	public Object getValue(String n) {
+		return null;
+	}
+
+	@Override
+	public void setValue(String n, Object v) {
+		
 	}
 }
