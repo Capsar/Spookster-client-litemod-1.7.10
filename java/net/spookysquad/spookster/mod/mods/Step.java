55b32f5d2289bd3d45660830ed89b9e8f106646d
TehNeon <tehneon1212@gmail.com>
2015-07-07 12:12:58 -0400

    Step is working, Legit Step is derpy when collided with wall that is more then 1 block tall.

--- java/net/spookysquad/spookster/mod/mods/Step.java
+++ java/net/spookysquad/spookster/mod/mods/Step.java
@@ -9,6 +9,7 @@ import net.spookysquad.spookster.mod.HasValues;
 import net.spookysquad.spookster.mod.Module;
 import net.spookysquad.spookster.mod.Type;
 import net.spookysquad.spookster.mod.HasValues.Value;
+import net.spookysquad.spookster.utils.PlayerUtil;
 import net.spookysquad.spookster.utils.Wrapper;
 
 import org.lwjgl.input.Keyboard;
@@ -22,7 +23,11 @@ public class Step extends Module implements HasValues {
 	public void onEvent(Event event) {
 		if(event instanceof EventPreMotion) {
 			if(LegitStep) {
-				
+				if(PlayerUtil.getPlayer().isCollidedHorizontally) {
+					if(PlayerUtil.getPlayer().onGround) {
+						PlayerUtil.getPlayer().jump();
+					}
+				}
 			}
 			
 			if(VanillaStep) {
@@ -43,7 +48,7 @@ public class Step extends Module implements HasValues {
 	public double stepHeight = 0.5;
 	
 	private String STEPHEIGHT = "Step Height", VANILLA = "Vanilla Step", LEGIT = "Legit step";
-	private List<Value> values = Arrays.asList(new Value[] { new Value(STEPHEIGHT, 1.0, 5.0, 0.01F),
+	private List<Value> values = Arrays.asList(new Value[] { new Value(STEPHEIGHT, 0.5D, 10D, 0.1F),
 			new Value(VANILLA, false, true), new Value(LEGIT, false, true) });
 
 	@Override
@@ -61,7 +66,7 @@ public class Step extends Module implements HasValues {
 
 	@Override
 	public void setValue(String n, Object v) {
-		if (n.equals(STEPHEIGHT)) stepHeight = (Math.round((Double) v * 100) / 100.0D);
+		if (n.equals(STEPHEIGHT)) stepHeight = (Math.round((Double) v * 10) / 10.0D);
 		else if (n.equals(VANILLA)) VanillaStep = (Boolean) v;
 		else if (n.equals(LEGIT)) LegitStep = (Boolean) v;
 	}
