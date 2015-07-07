8fa85fb31de15255fc6cfb084f24fd0c2f96b673
TehNeon <tehneon1212@gmail.com>
2015-07-07 10:20:13 -0400

    Added Show Invisible to Nametags

--- java/net/spookysquad/spookster/mod/mods/Nametag.java
+++ java/net/spookysquad/spookster/mod/mods/Nametag.java
@@ -42,6 +42,7 @@ public class Nametag extends Module implements HasValues {
 	public boolean sneak = true;
 	public boolean health = true;
 	public boolean friends = true;
+	public boolean invisibles = true;
 	public float scaleFactor = 1.6F;
 	public int scaleDistance = 16;
 
@@ -83,7 +84,7 @@ public class Nametag extends Module implements HasValues {
 		int var17 = fontRenderer.getStringWidth(name) / 2;
 		var15.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.6F);
 
-		if (entity.isInvisible()) {
+		if (entity.isInvisible() && invisibles) {
 			var15.setColorRGBA_F(0.0F, 0.4F, 1.0F, 0.6F);
 		}
 
@@ -103,10 +104,10 @@ public class Nametag extends Module implements HasValues {
 		GL11.glPopMatrix();
 	}
 
-	private String SNEAK = "Show Sneak", FRIENDS = "Show Friends", HEALTH = "Show Health", SCALEFACTOR = "Scale Factor",
+	private String SNEAK = "Show Sneak", FRIENDS = "Show Friends", HEALTH = "Show Health", INVISIBLE = "Show Invisible", SCALEFACTOR = "Scale Factor",
 			SCALEDISTANCE = "Scale Distance";
 	private List<Value> values = Arrays.asList(new Value[] { new Value(SNEAK, false, true), new Value(FRIENDS, false, true),
-			new Value(HEALTH, false, true), new Value(SCALEFACTOR, 0.1f, 4f, 0.1f), new Value(SCALEDISTANCE, 0, 32, 1) });
+			new Value(HEALTH, false, true), new Value(INVISIBLE, false, true), new Value(SCALEFACTOR, 0.1f, 4f, 0.1f), new Value(SCALEDISTANCE, 0, 32, 1) });
 
 	@Override
 	public List<Value> getValues() {
@@ -118,6 +119,7 @@ public class Nametag extends Module implements HasValues {
 		if (n.equals(SNEAK)) return sneak;
 		else if (n.equals(FRIENDS)) return friends;
 		else if (n.equals(HEALTH)) return health;
+		else if (n.equals(INVISIBLE)) return invisibles;
 		else if (n.equals(SCALEFACTOR)) return scaleFactor;
 		else if (n.equals(SCALEDISTANCE)) return scaleDistance;
 		return null;
@@ -128,6 +130,7 @@ public class Nametag extends Module implements HasValues {
 		if (n.equals(SNEAK)) sneak = Boolean.parseBoolean(v.toString());
 		else if (n.equals(FRIENDS)) friends = Boolean.parseBoolean(v.toString());
 		else if (n.equals(HEALTH)) health = Boolean.parseBoolean(v.toString());
+		else if (n.equals(INVISIBLE)) invisibles = Boolean.parseBoolean(v.toString());
 		else if (n.equals(SCALEFACTOR)) scaleFactor = (Float) v;
 		else if (n.equals(SCALEDISTANCE)) scaleDistance = (Integer) v;
 	}
