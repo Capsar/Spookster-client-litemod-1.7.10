30a8b33facd88c0fca0b7b0c0f975d7aac6ebbeb
TehNeon <tehneon1212@gmail.com>
2015-07-08 18:10:37 -0400

    Added Speed Blink.

--- java/net/spookysquad/spookster/mod/mods/Blink.java
+++ java/net/spookysquad/spookster/mod/mods/Blink.java
@@ -1,18 +1,22 @@
 package net.spookysquad.spookster.mod.mods;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import net.minecraft.network.play.client.C03PacketPlayer;
+import net.spookysquad.spookster.Spookster;
 import net.spookysquad.spookster.event.Event;
-import net.spookysquad.spookster.event.events.EventPacketGet;
 import net.spookysquad.spookster.event.events.EventPacketSend;
+import net.spookysquad.spookster.mod.HasValues;
 import net.spookysquad.spookster.mod.Module;
 import net.spookysquad.spookster.mod.Type;
+import net.spookysquad.spookster.mod.HasValues.Value;
 import net.spookysquad.spookster.utils.PacketUtil;
 
 import org.lwjgl.input.Keyboard;
 
-public class Blink extends Module {
+public class Blink extends Module implements HasValues {
 
 	private ArrayList<C03PacketPlayer> packets = new ArrayList<C03PacketPlayer>();
 	
@@ -20,12 +24,31 @@ public class Blink extends Module {
 		super(new String[] { "Blink" }, "POOF! You're gone", Type.MOVEMENT, Keyboard.KEY_LBRACKET, 0xFFaa676a);
 	}
 	
+	public boolean onEnable() {
+		if(speedBlink) {
+			if(Spookster.instance.moduleManager.getModule(Speed.class).isEnabled()) {
+				speedEnabled = true;
+			} else {
+				Spookster.instance.moduleManager.getModule(Speed.class).toggle(false);
+			}
+		}
+		
+		return super.onEnable();
+	}
+	
 	public boolean onDisable() {
 		for(C03PacketPlayer packet: packets) {
 			PacketUtil.sendPacket(packet);
 		}
 		
+		if(speedBlink) {
+			if(!speedEnabled) {
+				Spookster.instance.moduleManager.getModule(Speed.class).toggle(false);
+			}
+		}
+		
 		packets.clear();
+		speedEnabled = false;
 		return super.onDisable();
 	}
 
@@ -38,6 +61,29 @@ public class Blink extends Module {
 				event.cancel();
 			}
 		}
+	}
+	
+	
+	private boolean speedBlink = false;
+	private boolean speedEnabled = false;
+	
+	private String SPEEDBLINK = "Speed Blink";
+	private List<Value> values = Arrays.asList(new Value[] { new Value(SPEEDBLINK, false, true) });
+
+	@Override
+	public List<Value> getValues() {
+		return values;
+	}
+
+	@Override
+	public Object getValue(String n) {
+		if (n.equals(SPEEDBLINK)) return speedBlink;
+		return null;
+	}
+
+	@Override
+	public void setValue(String n, Object v) {
+		if (n.equals(SPEEDBLINK)) speedBlink = (Boolean) v;
 	}
 
 }
