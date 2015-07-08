9c27a07928894654dc16b949b87bc4ce8597ea43
TehNeon <tehneon1212@gmail.com>
2015-07-08 14:19:27 -0400

    Added null check to world for Fullbright

--- java/net/spookysquad/spookster/mod/mods/Fullbright.java
+++ java/net/spookysquad/spookster/mod/mods/Fullbright.java
@@ -15,37 +15,37 @@ import org.lwjgl.input.Keyboard;
 public class Fullbright extends Module implements HasValues {
 
 	private final float[] tempBrightnessTable = new float[16];
-	
+
 	public Fullbright() {
 		super(new String[] { "Fullbright" }, "", Type.RENDER, Keyboard.KEY_B, 0xFFE1E1C8);
 	}
-	
+
 	public boolean onEnable() {
-		if(getPlayer() == null)
-			return false;
-		
+		if (getPlayer() == null) return false;
+
 		System.arraycopy(Wrapper.getWorld().provider.lightBrightnessTable, 0, tempBrightnessTable, 0, 16);
 		return true;
 	}
-	
+
 	public boolean onDisable() {
-		if(getPlayer() == null)
-			return false;
-		
+		if (getPlayer() == null) return false;
+
 		System.arraycopy(tempBrightnessTable, 0, Wrapper.getWorld().provider.lightBrightnessTable, 0, 16);
 		return true;
 	}
 
 	public void onEvent(Event event) {
-		if(event instanceof EventGameTick) {
-			for(int i = 0; i < 16; i++) {
-				Wrapper.getWorld().provider.lightBrightnessTable[i] = brightness;
+		if (event instanceof EventGameTick) {
+			if (Wrapper.getWorld() != null) {
+				for (int i = 0; i < 16; i++) {
+					Wrapper.getWorld().provider.lightBrightnessTable[i] = brightness;
+				}
 			}
 		}
 	}
-	
+
 	public float brightness = 0.7F;
-	
+
 	private String BRIGHTNESS = "Brightness";
 	private List<Value> values = Arrays.asList(new Value[] { new Value(BRIGHTNESS, 0d, 1d, 0.01F) });
 
