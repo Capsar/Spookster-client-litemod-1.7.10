f5c036093271918284d712b73f37ee1ec5e8ad24
TehNeon <tehneon1212@gmail.com>
2015-07-14 18:43:27 -0400

    Dirka Dirka values

--- java/net/spookysquad/spookster/mod/mods/MobFarm.java
+++ java/net/spookysquad/spookster/mod/mods/MobFarm.java
@@ -1,6 +1,7 @@
 package net.spookysquad.spookster.mod.mods;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -11,12 +12,14 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.spookysquad.spookster.event.Event;
 import net.spookysquad.spookster.event.events.EventPreMotion;
+import net.spookysquad.spookster.mod.HasValues;
 import net.spookysquad.spookster.mod.Module;
 import net.spookysquad.spookster.mod.Type;
+import net.spookysquad.spookster.mod.values.Value;
 import net.spookysquad.spookster.utils.AngleUtil;
 import net.spookysquad.spookster.utils.TimeUtil;
 
-public class MobFarm extends Module {
+public class MobFarm extends Module implements HasValues {
 
 	public MobFarm() {
 		super(new String[] { "Mob Farm" }, "Farm mobs in your fieldview", Type.COMBAT, -1, 0xFF52c5ff);
@@ -59,19 +62,43 @@ public class MobFarm extends Module {
 		getPlayer().swingItem();
 		getController().attackEntity(getPlayer(), mob);
 		mobs.remove(mob);
-		time.resetAndAdd(new Random().nextInt(50));
+		time.resetAndAdd(new Random().nextInt(randomHitDelay));
 		return true;
 	}
 
 	private boolean isMobAttackable(EntityLivingBase mob) {
 
-		if (getPlayer().getDistanceToEntity(mob) < 4.0 && mob.hurtResistantTime <= 8 && mob.getHealth() > 0 && !mob.isDead) {
+		if (getPlayer().getDistanceToEntity(mob) < reach && mob.hurtResistantTime <= 8 && mob.getHealth() > 0 && !mob.isDead) {
 			float[] angles = AngleUtil.getAngles(mob, getPlayer());
 			float yaw = AngleUtil.getDistanceBetweenAngle(getPlayer().rotationYaw, angles[0]);
 			float pitch = AngleUtil.getDistanceBetweenAngle(getPlayer().rotationPitch, angles[1]);
 			if (yaw < 60 && pitch < 90) return true;
 		}
 		return false;
+	}
+	
+	public double reach = 4.0;
+	public int randomHitDelay = 50;
+	
+	private String REACH = "Hit Reach", RANDOMDELAY = "Random Hit Delay";
+	private List<Value> values = Arrays.asList(new Value[] { new Value(REACH, 1.0, 6.0, 0.1F), new Value(RANDOMDELAY, 1, 1000, 1F)});
+
+	@Override
+	public List<Value> getValues() {
+		return values;
+	}
+
+	@Override
+	public Object getValue(String n) {
+		if (n.equals(REACH)) return reach;
+		else if (n.equals(RANDOMDELAY)) return randomHitDelay;
+		return null;
+	}
+
+	@Override
+	public void setValue(String n, Object v) {
+		if (n.equals(REACH)) reach = (Math.round((Double) v * 10) / 10.0D);
+		else if (n.equals(RANDOMDELAY)) randomHitDelay = Math.round((Integer) v);
 	}
 
 }
--- java/net/spookysquad/spookster/mod/mods/Triggerbot.java
+++ java/net/spookysquad/spookster/mod/mods/Triggerbot.java
@@ -31,6 +31,7 @@ public class Triggerbot extends Module implements HasValues {
 	public int aps = 5;
 	public int aimSpeed = 2;
 	public int aimAssistSpeed = 2;
+	public int aimLimit = 60;
 	public double attackRange = 4.2;
 	public double smoothRange = 4.4;
 	public boolean swordOnly = true;
@@ -68,7 +69,7 @@ public class Triggerbot extends Module implements HasValues {
 				boolean canAttack = PlayerUtil.canAttack(entityTarget, 0, swordOnly, blockHit)
 						&& getPlayer().getDistanceToEntity(entityTarget) <= smoothRange
 						&& getPlayer().canEntityBeSeen(entityTarget);
-				boolean shouldStopToAim = AngleUtil.shouldAim(60, entityTarget);
+				boolean shouldStopToAim = AngleUtil.shouldAim(aimLimit, entityTarget);
 				if (!canAttack || aimAssistSpeed == 0) {
 					entityTarget = null;
 					return;
@@ -86,11 +87,11 @@ public class Triggerbot extends Module implements HasValues {
 	}
 
 	private String APS = "Attacks per second", AIMSPEED = "Aim Speed", AIMASSISTSPEED = "Aim Assist Speed",
-			ATTACKRANGE = "Attack Range", SMOOTHRANGE = "Smooth Range", SWORDONLY = "Swords only",
+			ATTACKRANGE = "Attack Range", AIMLIMIT = "Aim Limit", SMOOTHRANGE = "Smooth Range", SWORDONLY = "Swords only",
 			SMOOTHAIM = "Smooth aim assist", BLOCKHIT = "Block hit";
 	private List<Value> values = Arrays.asList(new Value[] { new Value(APS, 0, 20, 1),
 			new Value(ATTACKRANGE, 3.0, 6.0, 0.1F), new Value(SMOOTHRANGE, 3.0, 6.0, 0.1F), new Value(AIMSPEED, 0, 30, 1),
-			new Value(AIMASSISTSPEED, 0, 30, 1), new Value(SMOOTHAIM, false, true), new Value(SWORDONLY, false, true),
+			new Value(AIMASSISTSPEED, 0, 30, 1), new Value(AIMLIMIT, 1, 180, 1), new Value(SMOOTHAIM, false, true), new Value(SWORDONLY, false, true),
 			new Value(BLOCKHIT, false, true) });
 
 	@Override
@@ -108,6 +109,7 @@ public class Triggerbot extends Module implements HasValues {
 		else if (n.equals(SWORDONLY)) return swordOnly;
 		else if (n.equals(SMOOTHAIM)) return smoothAssist;
 		else if (n.equals(BLOCKHIT)) return blockHit;
+		else if (n.equals(AIMLIMIT)) return aimLimit;
 		return null;
 	}
 
@@ -116,6 +118,7 @@ public class Triggerbot extends Module implements HasValues {
 		if (n.equals(APS)) aps = (Integer) v;
 		else if (n.equals(AIMSPEED)) aimSpeed = (Integer) v;
 		else if (n.equals(AIMASSISTSPEED)) aimAssistSpeed = (Integer) v;
+		else if (n.equals(AIMLIMIT)) aimLimit = (Integer) v;
 		else if (n.equals(ATTACKRANGE)) attackRange = (Math.round((Double) v * 10) / 10.0D);
 		else if (n.equals(SMOOTHRANGE)) smoothRange = (Math.round((Double) v * 10) / 10.0D);
 		else if (n.equals(SWORDONLY)) swordOnly = (Boolean) v;
